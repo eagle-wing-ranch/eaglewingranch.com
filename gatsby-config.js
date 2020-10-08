2f5e7fcfab470964b8aba83e298482b8b85df939
Dane Stevens <dane@dsmedia.ca>
2020-10-08 09:07:59 -0400

    change to custom stats domain

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,7 @@ module.exports = {
       resolve: `gatsby-plugin-plausible`,
       options: {
         domain: `eaglewingranch.ca`,
-        // customDomain: 'stats.eaglewingranch.ca'
+        customDomain: 'stats.eaglewingranch.ca'
       },
     },
   ],
